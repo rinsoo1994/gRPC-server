3d3964a551b3f901411d00ed6237e64f67562501
rinsoo <rinsoo1994@gmail.com>
2021-09-21 14:28:03 +0900

    init

---
+++ calculator/calculator-client.py
@@ -0,0 +1,27 @@
+import logging
+import grpc
+
+import calculator_pb2
+import calculator_pb2_grpc
+
+
+def run():
+    with grpc.insecure_channel('localhost:50052') as channel:
+        stub = calculator_pb2_grpc.CalculateStub(channel)
+        print(calculator_pb2.CalculateRequest(x=1, y=2, input_num_values=[1, 2, 3, 4, 5], input_nums=[1, 2, 3, 4, 5]).SerializeToString)
+        print("zz", calculator_pb2.CalculateRequest(x=1, y=2, input_num_values=[1, 2, 3, 4, 5], input_nums=[1, 2, 3, 4, 5]))
+        response = stub.GetPlus(calculator_pb2.CalculateRequest(x=1, y=2, input_num_values=[1,2,3,4,5], input_nums=[1,2,3,4,5]))
+        print("GetPlus client received: " + response.result, response)
+        response = stub.GetMinus(calculator_pb2.CalculateRequest(x=1, y=2, input_num_values=[1,2,3,4,5], input_nums=[1,2,3,4,5]))
+        print("GetMinus client received: " + response.result)
+        response = stub.GetMultiply(calculator_pb2.CalculateRequest(x=1, y=2, input_num_values=[1,2,3,4,5], input_nums=[1,2,3,4,5]))
+        print("GetMultiply client received: " + response.result)
+        response = stub.GetDivision(calculator_pb2.CalculateRequest(x=1, y=2, input_num_values=[1,2,3,4,5], input_nums=[1,2,3,4,5]))
+        print("GetDivision client received: " + response.result)
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    run()
+
+
---
+++ calculator/calculator-server.py
@@ -0,0 +1,39 @@
+from concurrent import futures
+import logging
+import grpc
+
+
+import calculator_pb2
+import calculator_pb2_grpc
+
+
+class Calculator(calculator_pb2_grpc.CalculateServicer):
+    def GetPlus(self, request, context):
+        print(request.__dir__())
+        response = int(request.x) + int(request.y)
+        return calculator_pb2.CalculateReply(result=str(response))
+
+    def GetMinus(self, request, context):
+        response = int(request.x) - int(request.y)
+        return calculator_pb2.CalculateReply(result=str(response))
+
+    def GetMultiply(self, request, context):
+        response = int(request.x) * int(request.y)
+        return calculator_pb2.CalculateReply(result=str(response))
+
+    def GetDivision(self, request, context):
+        response = int(request.x) / int(request.y)
+        return calculator_pb2.CalculateReply(result=str(response))
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    calculator_pb2_grpc.add_CalculateServicer_to_server(Calculator(), server)
+    server.add_insecure_port('[::]:50052')
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    serve()
